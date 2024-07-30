a97e04d674b8df3864345cbd57ac1b19972ce3e3
johoit <mirai.itsys+kaken@gmail.com>
2024-07-30 15:09:26 +0900

    migration

---
+++ drizzle/migrations/0000_abandoned_hitman.sql
@@ -0,0 +1,22 @@
+CREATE TABLE `locate` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text(100) NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE `count_log` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`count` integer NOT NULL,
+	`timestamp` text DEFAULT (current_timestamp) NOT NULL,
+	`locate_id` integer NOT NULL,
+	`src_type` integer NOT NULL,
+	FOREIGN KEY (`locate_id`) REFERENCES `locate`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`src_type`) REFERENCES `src_type`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `src_type` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text(100) NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `locate_name_unique` ON `locate` (`name`);--> statement-breakpoint
+CREATE UNIQUE INDEX `src_type_name_unique` ON `src_type` (`name`);
