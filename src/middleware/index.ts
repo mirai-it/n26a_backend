9dff00148199726461b238dd1bc171677297f3c0
johoit <mirai.itsys+kaken@gmail.com>
2024-08-15 10:27:40 +0900

    JSONの解析を一律にした

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,12 +1,4 @@
-import { MiddlewareHandler } from "hono";
-import { jwt } from "hono/jwt";
+import authMiddleware from "./auth";
+import jsonMiddleware from "./jsonParse";
 
-const authMiddleware: MiddlewareHandler = async (c, next) => {
-  if (c.req.method !== "GET") {
-    // 認証ロジックをここに実装
-    return await jwt({ secret: c.env.AUTH_SECRET })(c, next);
-  }
-  await next();
-};
-
-export default authMiddleware;
+export { authMiddleware, jsonMiddleware };
---
+++ src/middleware/jsonParse.ts
@@ -0,0 +1,12 @@
+import { Context, Next } from "hono";
+
+export default async function jsonMiddleware(c: Context, next: Next) {
+  if (["POST", "PUT", "PATCH"].includes(c.req.method)) {
+    try {
+      await c.req.json();
+    } catch (e) {
+      return c.json({ error: "Invalid JSON in request body" }, 400);
+    }
+  }
+  await next();
+}
