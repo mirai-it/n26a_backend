793884b494d00e122e18c77defc5079818e33a01
johoit <mirai.itsys+kaken@gmail.com>
2024-08-15 12:35:42 +0900

    削除処理をわかりやすく

--- src/srcType/delete.ts
+++ src/srcType/delete.ts
@@ -3,16 +3,45 @@ import { srcType } from "../db/schema";
 import { eq } from "drizzle-orm";
 import { Context } from "hono";
 
-const deleteSrcType = async (c: Context) => {
-  const id = parseInt(c.req.param("id"), 10);
-  console.log(id);
-  const db = drizzle(c.env.DB);
-  await db.delete(srcType).where(eq(srcType.id, id));
+const validateId = (id: string): number | null => {
+  const numericId = parseInt(id);
+  return Number.isInteger(numericId) ? numericId : null;
+};
 
+const deleteSrcType = async (c: Context) => {
+  const id = validateId(c.req.param("id"));
+
+  // IDの検証
+  if (id === null) {
+    return c.json(
+      {
+        error: "Invalid ID",
+        message: "The provided ID must be a integer.",
+      },
+      400
+    );
+  }
+
+  const db = drizzle(c.env.DB);
+  const onErr = await db
+    .delete(srcType)
+    .where(eq(srcType.id, id))
+    .then(() => false)
+    .catch(() => true);
+
+  if (onErr) {
+    return c.json(
+      {
+        message: "Source type deleted faild",
+        timestamp: new Date().toISOString(),
+      },
+      500
+    );
+  }
   return c.json(
     {
-      result: "OK",
-      time: new Date().toISOString(),
+      message: "Source type deleted successfully",
+      timestamp: new Date().toISOString(),
     },
     200
   );
